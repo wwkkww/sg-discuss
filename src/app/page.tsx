12211a8f1c101d18ccffed641ae95fb0d1961418
Kevin <wongkokwah@gmail.com>
2024-02-17 14:44:56 +0800

    setup header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Button, divider } from "@nextui-org/react";
 import * as actions from "@/actions";
 import { auth } from "@/auth";
-import Profile from "./components/profile";
+import Profile from "@/components/profile";
 
 export default async function Home() {
   const session = await auth();
