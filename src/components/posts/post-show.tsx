1c5d045b6f174974455f2859e3403e93819c9d36
kevin <wongkokwah@gmail.com>
2024-02-18 23:07:49 +0800

    add comments feature

--- src/components/posts/post-show.tsx
+++ src/components/posts/post-show.tsx
@@ -1,6 +1,17 @@
-interface PostShowProps {}
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+interface PostShowProps {
+  postId: string;
+}
 
-export default function PostShow({}: PostShowProps) {
+export default async function PostShow({ postId }: PostShowProps) {
+  const post = await db.post.findFirst({
+    where: { id: postId },
+  });
+
+  if (!post) {
+    notFound();
+  }
   return (
     <div className="m-4">
       <h1 className="text-2xl font-bold my-2">{post.title}</h1>
